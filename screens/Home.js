b89231bed050a7f736637223c94c04d653a28c72
camfroth <cameron.roth@outlook.com>
2022-09-18 22:48:19 +0800

    Committing to code on PC, not finished yet lol

--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,66 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-function Home() {
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Button, Alert, Modal , TextInput, TouchableOpacity } from 'react-native';
+import {View,Text} from 'react-native';
+import React, { useState } from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import AutoLog from './AutoLog.js';
+import ManualLog from './ManualLog.js';
+
+const Stack = createNativeStackNavigator();
+
+function Home({navigation}) {
     return (
-
-        // Map Interface
-
-        <View style={{alignItems:'center', justifyContent:'center', flex:1}}>
-            <Text>Home Screen</Text>
-        </View>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <View style={{alignItems:'center', justifyContent:'center', flex:1}}>
+                    <View style = {styles.div}></View>
+                    <Text>Home Screen</Text>
+                    <TouchableOpacity
+                        onPress={() => alert('hello world!')}
+                        style={styles.button}>
+                        <Text style={{ fontSize: 20, textAlign: 'center', color:'#fff'}}>Start Automatic tracking</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => alert('hello world!')}
+                        style={styles.button}>
+                        <Text style={{ fontSize: 20, color: '#fff' }}>Manual Log</Text>
+                    </TouchableOpacity>
+                    <View style = {styles.div}></View>
+                </View>
+            </Stack.Navigator>
+        </NavigationContainer>
 
         // 
     )
 }
 export default Home
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    height:40,
+    borderColor: 'black',
+    borderWidth: 2
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#A9A9A9',
+    padding: 10,
+    borderRadius: 10,
+    width: 250,
+    marginTop: 10,
+    height: 100,
+    flex: 1
+  },
+  div: {
+    flex:1
+  }
+});
---
+++ screens/ManualLog.js
@@ -0,0 +1,46 @@
+// Create basic React Native app page
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Button, Alert, Modal , TextInput, TouchableOpacity } from 'react-native';
+import {View,Text} from 'react-native';
+import React, { useState } from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function ManualLog() {
+    return (
+        <View style={styles.container}>
+        <Text>Manual Log</Text>
+        </View>
+    );
+    }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    input: {
+        height:40,
+        borderColor: 'black',
+        borderWidth: 2
+    },
+    button: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#A9A9A9',
+        padding: 10,
+        borderRadius: 10,
+        width: 250,
+        marginTop: 10,
+        height: 100,
+        flex: 1
+    },
+    div: {
+        flex:1
+    }
+    });
+
+export default ManualLog
