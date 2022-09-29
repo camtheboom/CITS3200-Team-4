c0de81f9e29d045b15b92be78f113f3d4af3abac
thelonebraincell <83766601+kaiyork@users.noreply.github.com>
2022-09-29 15:33:57 +0800

    Update location.js

--- location.js
+++ location.js
@@ -1,15 +1,21 @@
+
 import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import MapView from 'react-native-maps';
 import { Marker } from 'react-native-maps';
+import PermissionsButton from "./Permission";
+import useEffect from 'react';
 const App = () => {
+
+
     const [mapRegion, setmapRegion] = useState({
         latitude: 37.78825,
         longitude: -122.4324,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
     });
-  eturn (
+
+    return (
         <View style={styles.container}>
             <MapView
                 style={{ alignSelf: 'stretch', height: '100%' }}
@@ -37,9 +43,18 @@ const App = () => {
 
 
 };
-    }, []);
 
-
-};
+export default App;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paragraph: {
+        fontSize: 16,
+        textAlign: 'center',
+    },
+});
 
 
