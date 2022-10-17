19f874fd3b99313668255bd79db0fa42428c125c
camtheboom <101784910+camtheboom@users.noreply.github.com>
2022-10-17 17:40:38 +0800

    Changed travel log
    travel log should be able to use data and display

--- screens/TravelLog.js
+++ screens/TravelLog.js
@@ -1,30 +1,81 @@
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import { useRoute } from '@react-navigation/native';
-  
-const TravelLog = () => {
-  const route = useRoute();
-  
+import { SectionList, StyleSheet, Text, View } from 'react-native';
+export default function TravelLog() {
+  const newTaskData = [{
+    title: "Past Logs",
+    data: [
+      {
+        desciption: 'skrdsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+        start_location: 'gym',
+        end_location: 'home',
+        method_of_movement:'car'
+      },
+      {
+        desciption: 'a',
+        start_location: 'gym',
+        end_location: 'home',
+        method_of_movement:'car'
+      },
+      {
+        desciption: 'descfweifwe',
+        start_location: 'gym',
+        end_location: 'home',
+        method_of_movement:'car'
+      },
+      {
+        desciption: 'sdmaskdasmdm',
+        start_location: 'gym',
+        end_location: 'home',
+        method_of_movement:'car'
+      },
+      {
+        desciption: 'dsdsadasda',
+        start_location: 'gym',
+        end_location: 'home',
+        method_of_movement:'car'
+      },
+    ]
+  }];
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>please work</Text>
+      <SectionList
+        sections={[...newTaskData]}
+        renderItem={({item})=>(
+            <Text style={styles.taskItem}> 
+            Mode of Transport: {item.method_of_movement} {'\n'}
+            Start Location: {item.start_location} {'\n'}
+            End Location: {item.end_location} {'\n'}
+            Description: {item.desciption} 
+            </Text>
+        )}
+        
+        renderSectionHeader={({section})=>(
+          <Text style={styles.taskTitle}>{section.title}</Text>
+        )}
+        keyExtractor={item=>item.id}
+        stickySectionHeadersEnabled
+      />
     </View>
   );
-};
-
-export default TravelLog;
-
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#eafffe'
   },
-  title: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    color: 'green',
-    marginTop: 50,
+  taskItem:{
+    padding: 10,
+    marginVertical: 15,
+    fontSize: 16
   },
+  taskTitle:{
+    backgroundColor: "#ffffff",
+    fontSize: 20,
+    fontWeight: "bold",
+    padding: 10,
+    elevation: 4,
+    margin: 10,
+    marginBottom: 0,
+    borderRadius: 10
+  }
 });
